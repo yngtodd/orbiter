7e846f76f8d8bb69b1bd97b5dc90cd194315d79d
yngtodd <young.todd.mk@gmail.com>
2018-12-02 15:56:00 -0500

    fix typo

--- orbiter/rendering/shadow.py
+++ orbiter/rendering/shadow.py
@@ -1,7 +1,7 @@
 import pygame as pg
 
 
-def cast_shado(screen):
+def cast_shadow(screen):
     """Add optional terminator and shadow behind planet to screen."""
     shadow = pg.Surface((400, 100), flags=pg.SRCALPHA) # (Width, Height)
     shadow.fill((0, 0, 0, 210))
